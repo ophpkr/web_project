57d461ded981c44f0ebe8ff9e5f8df17946ea87e
ophpkr <ophelie.piekarek@etu.umontpellier.fr>
2017-05-19 01:17:59 +0200

    color ok

--- Vue/menu.php
+++ Vue/menu.php
@@ -1,8 +1,23 @@
+    
+<div>  
     <ul id= "nav-side" class= "side-nav fixed col s3  brown darken-1" >
         <li class= "logo">
             <a href= "tournament_management.php" class= "brand-logo">yo</a>
         </li>
-        <li class= "bold">
-            <a href= "#" class= "brand-logo">coucou</a>
+        
+        <li class="no-padding">
+          <ul class="collapsible collapsible-accordion">
+            <li class="bold"><a class="collapsible-header waves-effect waves-teal">CSS</a>
+              <div class="collapsible-body ">
+                <ul>
+                  <li class= "brown lighten-1"><a href="#">Color</a></li>
+                  <li class= "brown lighten-1"><a href="#">Grid</a></li>
+                  <li class= "brown lighten-1"><a href="#">Helpers</a></li>
+                  <li class= "brown lighten-1"><a href="#">Media</a></li>
+                  <li class= "brown lighten-1"><a href="#">Pulse</a></li>
+                </ul>
+              </div>
+            </li>
         </li>
     </ul>
+</div>  
